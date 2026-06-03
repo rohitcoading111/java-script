a43686a99638ab5bd61d3c59cb745053800c0a70
rohitcoading111 <rp538220@gmail.com>
2026-06-03 17:06:54 +0530

    using include solved this question

--- vovels.js
+++ vovels.js
@@ -1,16 +1,14 @@
-let vowels = function(str){
+let vovels = function(a){
     let count = 0;
-    let vowelList = ["a", "e", "i", "o", "u"];
-    for(let i = 0; i < str.length; i++){
-
-        let char = str[i];
-
-        if(vowelList.includes(char)){
+    let vovels = ["a","e","i","o","u"];
+    for(let i = 0; i<a.length; i++){
+        let char = a[i];
+        if(vovels.includes(char)){
             count++;
         }
     }
 
     return count;
+    
 }
-
-console.log(vowels("rohit"));
+console.log(vovels("aeiou"));
