408ddc785058d1cfabf910649d26f33628f636f8
rohitcoading111 <rp538220@gmail.com>
2026-06-08 15:31:39 +0530

    reversing an array

---
+++ reverse.js
@@ -0,0 +1,9 @@
+function reverse(){
+    let arr = [1,2,3,4,5,6,7,8,9];
+    let result =  arr.reverse();
+    for(let i = 0; i<arr.length; i++){
+        console.log(result[i]);
+        
+    }
+}
+reverse()
