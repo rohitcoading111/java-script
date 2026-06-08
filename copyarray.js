68b22227f351c94c2b7422449a264cd0b18adab0
rohitcoading111 <rp538220@gmail.com>
2026-06-08 16:11:40 +0530

    copy array using slice

---
+++ copyarray.js
@@ -0,0 +1,8 @@
+function copying(){
+    let arr = [1,2,3,5,4,6,7,8,9];
+    let result = arr.slice(0,6)
+    for(let i = 0; i<result.length; i++){
+        console.log(result[i]);
+    }
+}
+copying()
