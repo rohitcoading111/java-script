fdfebd4b3efc08ff828cfe7d3bedef769e7c09b4
rohitcoading111 <rp538220@gmail.com>
2026-06-10 22:32:42 +0530

    pass strength cheaker

---
+++ passwordstrength.js
@@ -0,0 +1,19 @@
+function checkPassword(password) {
+    let score = 0;
+
+    if (password.length >= 8) score++;
+    if (/[A-Z]/.test(password)) score++;
+    if (/[a-z]/.test(password)) score++;
+    if (/[0-9]/.test(password)) score++;
+    if (/[^A-Za-z0-9]/.test(password)) score++;
+
+    if (score <= 2) {
+        return "Weak Password";
+    } else if (score <= 4) {
+        return "Medium Password";
+    } else {
+        return "Strong Password";
+    }
+}
+
+console.log(checkPassword("Rohit@123"));
