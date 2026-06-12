98b354994538e1be1675d68f41e40306bf101e27
rohitcoading111 <rp538220@gmail.com>
2026-06-12 22:40:07 +0530

    calculator

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function calculate(num1, num2, operator) {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    case '/':
+      return num2 !== 0 ? num1 / num2 : "Cannot divide by zero";
+    default:
+      return "Invalid operator";
+  }
+}
+console.log(calculate(10, 5, '+')); // 15
