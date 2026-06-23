b6c15dd3568a8e18ef16586df0bb3226d88e1f63
rohitcoading111 <rp538220@gmail.com>
2026-06-23 22:27:33 +0530

    student result system completed

--- student.js
+++ student.js
@@ -15,3 +15,16 @@ for (let student of students) {
 
 console.log("Topper:", topper);
 
+let total = 0;
+
+for (let student of students) {
+  total += student.marks;
+}
+
+console.log("Average:", total / students.length);
+
+let pass = students.filter(student => student.marks >= 40);
+console.log("Pass:", pass);
+
+let sorted = [...students].sort((a, b) => b.marks - a.marks);
+console.log(sorted);
